c297bc274450c30e1458030730d5dc3d035a0cbe
Joaquin <joaquinsepulveda@hotmail.com>
2021-08-22 16:52:51 -0400

    Revert "FIX Production error"
    This reverts commit edce5af32821c9730dc04e9e421c90250f550a3d.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   end
 
   # Mailer
-  config.action_mailer.default_url_options = { host: 'planeat-cl.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'planeat.xyz' }
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
           api_key: ENV['MAILGUN_API_KEY'],
