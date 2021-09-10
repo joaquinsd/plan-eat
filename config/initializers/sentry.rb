92825488c033e940feb16d178175769a77e95d78
Joaquin <joaquinsepulveda@hotmail.com>
2021-09-10 15:48:41 -0300

    ADD ENV VAR for Sentry DSN

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = 'https://examplePublicKey@o0.ingest.sentry.io/0'
+  config.dsn = ENV['SENTRY_DSN']
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # To activate performance monitoring, set one of these options.
